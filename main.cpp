aeb088bcf1caf0b9a7281076353af51fdf5fb039
Thad Selby <tjselby@nic.edu>
2025-03-25 14:03:11 -0700

    making sure that aliens dont feel bad here

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main()
     //Joseph
     std::cout << "Hello - this is Joseph's code\n";
     std::cout << "This is the second line that my code displays\n";
-
+    std::cout<<"This may or may not be the thrid line\n";
 
     return 0;
 }
